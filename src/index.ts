57c160b437eb185da35f6ba46cc1cfbc56335104
Daniel Beckwith <daniel.beckwith@dazn.com>
2021-03-05 16:00:56 +0000

    Basic GQL endpoint which returns hardcoded array of alerts

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,37 @@
-import { ApolloServer } from 'apollo-server';
+import { ApolloServer, gql } from 'apollo-server';
 
 console.log('Henk');
 
-const server = new ApolloServer({});
+const typeDefs = gql`
+  type Artist {
+    name: String
+    genre: String
+  }
 
-console.log(server);
+  type Query {
+    artists: [Artist]
+  }
+`;
+
+const artists = [
+  {
+    name: 'Mental Cruelty',
+    genre: 'Slamming Deathcore',
+  },
+  {
+    name: '100 Gecs',
+    genre: 'Hyperpop',
+  },
+];
+
+const resolvers = {
+  Query: {
+    artists: () => artists,
+  },
+};
+
+const server = new ApolloServer({ typeDefs, resolvers });
+
+server.listen().then(({ url }) => {
+  console.log('HELLO BIG BOY');
+});
